2411a5048e5a33814ee407fcf339f239c76c8929
mj8350 <mj8350@naver.com>
2024-03-18 16:50:09 +0900

    240318작업1

--- Assets/Script/Arrow.cs
+++ Assets/Script/Arrow.cs
@@ -16,4 +16,13 @@ public class Arrow : PoolLabel
     {
         transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<IDamage>(out IDamage damage))
+        {
+            damage.TakeDamage(5);
+            ReturnPool();
+        }
+    }
 }
--- Assets/Script/MonsterChar.cs
+++ Assets/Script/MonsterChar.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterChar : PoolLabel
+public class MonsterChar : PoolLabel, IDamage
 {
-    
+    public void TakeDamage(float damage)
+    {
+        Debug.Log($"몬스터가 {damage}의 데미지를 입음");
+    }
 }
